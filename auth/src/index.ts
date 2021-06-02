82c82565f31e0e4af92b3c8a0145466be6460382
yanchen01 <ychen01@bu.edu>
2021-06-02 01:22:05 -0400

    added custom error handlers and mongo instance for auth service

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,12 +1,43 @@
 import express from 'express';
+import 'express-async-errors';
+import mongoose from 'mongoose';
+
+import { currentUserRouter } from './routes/current-user';
+import { signinRouter } from './routes/signin';
+import { signoutRouter } from './routes/signout';
+import { signupRouter } from './routes/signup';
+import { errorHandler } from './middlewares/error-handler';
+import { NotFoundError } from './errors/not-found-error';
 
 const app = express();
 app.use(express.json());
 
-app.get('/api/users/currentuser', (req, res) => {
-	res.send('Hi there!');
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(signupRouter);
+
+app.all('*', async (req, res) => {
+	throw new NotFoundError();
 });
 
-app.listen(3000, () => {
-	console.log('Listening on port 3000!');
-});
+app.use(errorHandler);
+
+const start = async () => {
+	try {
+		await mongoose.connect('mongodb://auth-mongo-srv:27017/auth', {
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+			useCreateIndex: true
+		});
+		console.log('Connected to MongoDB!');
+	} catch (err) {
+		console.log(err);
+	}
+
+	app.listen(3000, () => {
+		console.log('Listening on port 3000!');
+	});
+};
+
+start();
