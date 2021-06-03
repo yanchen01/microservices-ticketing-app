efacca623cf8fd77af64f988893c63d74039ecd7
yanchen01 <ychen01@bu.edu>
2021-06-03 00:43:10 -0400

    completed auth service

--- auth/src/services/password.ts
+++ auth/src/services/password.ts
@@ -8,12 +8,12 @@ export class Password {
 		const salt = randomBytes(8).toString('hex');
 		const buf = (await scryptAsync(password, salt, 64)) as Buffer;
 
-		return `${buf.toString('hex')}.${salt}}`;
+		return `${buf.toString('hex')}.${salt}`;
 	}
 
-	static async compare(storedPassword: string, supliedPassword: string) {
+	static async compare(storedPassword: string, suppliedPassword: string) {
 		const [ hashedPassword, salt ] = storedPassword.split('.');
-		const buf = (await scryptAsync(supliedPassword, salt, 64)) as Buffer;
+		const buf = (await scryptAsync(suppliedPassword, salt, 64)) as Buffer;
 
 		return buf.toString('hex') === hashedPassword;
 	}
